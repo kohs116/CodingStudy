124eac3fd10d710ad8b0a166e5457214ab597516
kohs116 <kohs116@naver.com>
2023-02-27 23:53:17 +0900

    Dynamic Programming

---
+++ Algorithm/Dynamic programming/max.py
@@ -0,0 +1,7 @@
+n, m = map(int, input().split())
+dy = [0] * (m + 1)
+for i in range(n):
+    ps, pt = map(int, input().split())
+    for j in range(m, pt - 1, -1):
+        dy[j] = max(dy[j], dy[j - pt] + ps)  # pt 시간 소비
+print(dy[m])
